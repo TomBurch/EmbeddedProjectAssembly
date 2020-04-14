6eef6d8bd9b027cefc240c069cec76092ad5581c
TomBurch <thomas.burch1306@gmail.com>
2020-04-14 15:20:14 +0100

    Cleared part1 file

--- Part1-TrafficLight/Part1-TrafficLight.ino
+++ Part1-TrafficLight/Part1-TrafficLight.ino
@@ -1,60 +1,36 @@
 extern "C" {
-  void clear_all();
-  void stage_1(int);
-  void stage_2(int);
-  void stage_3(int);
-  void stage_4(int);
+  void stage_1();
+  void stage_2();
+  void stage_3();
+  void stage_4();
 }
 
-void setup() {
+void setup {
   asm volatile (
-    " .equ  GN_1, 0x01  \n"
-    " .equ  YE_1, 0x02  \n"
-    " .equ  RD_1, 0x04  \n"
-    " .equ  GN_2, 0x08  \n"
-    " .equ  YE_2, 0x10  \n"
-    " .equ  RD_2, 0x20  \n"  
-    " ldi r16, RD_1 + YE_1 + GN_1 + RD_2 + YE_2 + GN_2  \n"
-    " out 0x10, r16  \n"
-    " ldi r18, GN_1 + RD_2  \n"
-    " out 0x11, r18 \n"
+    ".equ GN, 0x01          \n"
+    ".equ YE, 0x02          \n"
+    ".equ RD, 0x04          \n"
+    "ldi r16, RD + YE + GN  \n"
+    "out 0x10, r16          \n"
+    "ldi r18, GN            \n"
+    "out 0x11, r18          \n"
     ::: "r16", "r18");
-  //Serial.begin(9600);
-  //Light1
 }
 
-const int TLight_1 = 1;
-const int TLight_2 = 2;
-
-void loop() {
-  delay(1000);  
-  stage_3(1);
+void loop {
+  stage_3();
   delay(1000);
-  stage_4(1);
+  stage_4();
   delay(1000);
-  stage_1(1);
-
-  delay(1000);
-
-  stage_1(2);
-  delay(1000);
-  stage_2(2);
-  delay(1000);
-  stage_3(2);
-
-  delay(1000);
-
-  stage_3(2);
-  delay(1000);
-  stage_4(2);
-  delay(1000);
-  stage_1(2);
+  stage_1();
   
   delay(1000);
 
-  stage_1(1);
+  stage_1();
   delay(1000);
-  stage_2(1);
+  stage_2();
   delay(1000);
-  stage_3(1);
+  stage_3();
+  
+  delay(1000):
 }
