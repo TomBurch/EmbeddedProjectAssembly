d26c9cead16a885daa400b1b9f02518ba006d0ed
TomBurch <thomas.burch1306@gmail.com>
2020-04-16 17:59:45 +0100

    Moved setup to .S file, added redToGreen() and greenToRed()

--- Part2-Intersection/Part2-Intersection.ino
+++ Part2-Intersection/Part2-Intersection.ino
@@ -1,55 +1,38 @@
 extern "C" {
+  void start();
   void stage(int, int);
 }
 
 void setup() {
-  asm volatile (
-    " .equ  GN_1, 0x01  \n"
-    " .equ  YE_1, 0x02  \n"
-    " .equ  RD_1, 0x04  \n"
-    " .equ  GN_2, 0x08  \n"
-    " .equ  YE_2, 0x10  \n"
-    " .equ  RD_2, 0x20  \n"  
-    " ldi r16, RD_1 + YE_1 + GN_1 + RD_2 + YE_2 + GN_2  \n"
-    " out 0x10, r16  \n"
-    " ldi r18, GN_1 + RD_2  \n"
-    " out 0x11, r18 \n"
-    ::: "r16", "r18");
+  start();
 }
 
 const int TLIGHT_1 = 0;
 const int TLIGHT_2 = 1;
 
+void redToGreen(int TLight) {
+  stage(TLight, 0);
+  delay(1000);
+  stage(TLight, 1);
+  delay(1000);
+  stage(TLight, 2);
+}
+
+void greenToRed(int TLight) {
+  stage(TLight, 2);
+  delay(1000);
+  stage(TLight, 3);
+  delay(1000);
+  stage(TLight, 0);
+}
+
 void loop() {
-  stage(TLIGHT_1, 2); //GN -> RD
+  greenToRed(TLIGHT_1);
+  //delay(500);
+  redToGreen(TLIGHT_2);
   delay(1000);
-  stage(TLIGHT_1, 3);
-  delay(1000);
-  stage(TLIGHT_1, 0);
-  
-  delay(1000);
-
-  stage(TLIGHT_2, 0); //RD -> GN
-  delay(1000);
-  stage(TLIGHT_2, 1);
-  delay(1000);
-  stage(TLIGHT_2, 2);
-
-  delay(1000);
-
-  stage(TLIGHT_2, 2); //GN -> RD
-  delay(1000);
-  stage(TLIGHT_2, 3);
-  delay(1000);
-  stage(TLIGHT_2, 0);
-  
-  delay(1000);
-
-  stage(TLIGHT_1, 0); //RD -> GN
-  delay(1000);
-  stage(TLIGHT_1, 1);
-  delay(1000);
-  stage(TLIGHT_1, 2);
-  
+  greenToRed(TLIGHT_2);
+  //delay(500);
+  redToGreen(TLIGHT_1);
   delay(1000);
 }
