321252836402cd6337f0bd1c782cd66d7c5df2cb
TomBurch <thomas.burch1306@gmail.com>
2020-04-18 15:34:36 +0100

    Added Part3, added Pedestrian lights

---
+++ Part3-Crossing/Part3-Crossing.ino
@@ -0,0 +1,37 @@
+extern "C" {
+  void start();
+  void stage(int, int);
+}
+
+void setup() {
+  start();
+}
+
+const int TLIGHT_1 = 0;
+const int TLIGHT_2 = 1;
+//const int PEDLIGHT;
+
+void redToGreen(int TLight) {
+  stage(TLight, 0);
+  delay(1000);
+  stage(TLight, 1);
+  delay(1000);
+  stage(TLight, 2);
+}
+
+void greenToRed(int TLight) {
+  stage(TLight, 2);
+  delay(1000);
+  stage(TLight, 3);
+  delay(1000);
+  stage(TLight, 0);
+}
+
+void loop() {
+  greenToRed(TLIGHT_1);
+  redToGreen(TLIGHT_2);
+  delay(2000);          //Traffic flowing, TLight_2
+  greenToRed(TLIGHT_2);
+  redToGreen(TLIGHT_1);
+  delay(2000);          //Traffic flowing, TLight_1
+}
