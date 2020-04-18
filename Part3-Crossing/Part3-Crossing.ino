92dcb2a53d53261b47351a2ca87b666d42e4ebf4
TomBurch <thomas.burch1306@gmail.com>
2020-04-18 16:16:51 +0100

    Labelled stages, changed ints to bytes

--- Part3-Crossing/Part3-Crossing.ino
+++ Part3-Crossing/Part3-Crossing.ino
@@ -8,36 +8,43 @@ void setup() {
   start();
 }
 
-const int TLIGHT_1 = 0;
-const int TLIGHT_2 = 1;
-const int PLIGHT   = 2;
+const byte TLIGHT_1 = 0;
+const byte TLIGHT_2 = 1;
+const byte PLIGHT   = 2;
+const byte T_RD     = 0;
+const byte T_RD_YE  = 1;
+const byte T_GN     = 2;
+const byte T_YE     = 3;
+const byte P_RD     = 0;
+const byte P_GN     = 1;
+const byte P_OFF    = 2;
 
 void redToGreen() {
   flashGreen();
-  stage(PLIGHT, 0);
+  stage(PLIGHT, P_RD);
   delay(500);
-  stage(TLIGHT_1, 1);
-  stage(TLIGHT_2, 1);
+  stage(TLIGHT_1, T_RD_YE);
+  stage(TLIGHT_2, T_RD_YE);
   delay(1000);
-  stage(TLIGHT_1, 2);
-  stage(TLIGHT_2, 2);
+  stage(TLIGHT_1, T_GN);
+  stage(TLIGHT_2, T_GN);
 }
 
 void greenToRed() {
-  stage(TLIGHT_1, 3);
-  stage(TLIGHT_2, 3);
+  stage(TLIGHT_1, T_YE);
+  stage(TLIGHT_2, T_YE);
   delay(1000);
-  stage(TLIGHT_1, 0);
-  stage(TLIGHT_2, 0);
+  stage(TLIGHT_1, T_RD);
+  stage(TLIGHT_2, T_RD);
   delay(500);
-  stage(PLIGHT, 1);
+  stage(PLIGHT, P_GN);
 }
 
 void flashGreen() {
   for (int i = 0; i < 5; i++) {
-    stage(PLIGHT, 1);
+    stage(PLIGHT, P_GN);
     delay(200);
-    stage(PLIGHT, 2);
+    stage(PLIGHT, P_OFF);
     delay(200);
   }
 }
