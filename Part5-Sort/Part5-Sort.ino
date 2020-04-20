b21331af89279766d0172082257c38687938a0f4
TomBurch <thomas.burch1306@gmail.com>
2020-04-20 17:49:27 +0100

    Changed int array to byte, added serial input

--- Part5-Sort/Part5-Sort.ino
+++ Part5-Sort/Part5-Sort.ino
@@ -1,13 +1,28 @@
 extern "C" {
+  start();
+}
 
+void generateArray(byte randArray[]) {
+  for (int i = 0; i < 50; i++) {
+    byte randByte = random(0, 256);
+    randArray[i] = randByte;
+  }
 }
 
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(0));
   start();
 }
 
+byte randArray[50];
 char command;
 
 void loop() {
+  while(!Serial.available()) {}
+  command = Serial.read();
+  if (command == 'c') {
+    generateArray(randArray);
+  }
+  command = '\0';
 }
