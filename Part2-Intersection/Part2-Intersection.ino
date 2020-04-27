8192ba258dd747c33d7ed2f9b7dbeaf20b18bbfa
TomBurch <thomas.burch1306@gmail.com>
2020-04-27 22:53:35 +0100

    Part2 finished

--- Part2-Intersection/Part2-Intersection.ino
+++ Part2-Intersection/Part2-Intersection.ino
@@ -9,32 +9,33 @@ void setup() {
 
 const byte TLIGHT_1 = 0;
 const byte TLIGHT_2 = 1;
-const byte T_RD     = 0;
-const byte T_RD_YE  = 1;
-const byte T_GN     = 2;
-const byte T_YE     = 3;
+const byte RD       = 0;
+const byte RD_YE    = 1;
+const byte GN       = 2;
+const byte YE       = 3;
 
 void redToGreen(int TLight) {
-  stage(TLight, T_RD);
+  stage(TLight, RD_YE);
   delay(1000);
-  stage(TLight, T_RD_YE);
-  delay(1000);
-  stage(TLight, T_GN);
+  stage(TLight, GN);
 }
 
 void greenToRed(int TLight) {
-  stage(TLight, T_GN);
+  stage(TLight, YE);
   delay(1000);
-  stage(TLight, T_YE);
-  delay(1000);
-  stage(TLight, T_RD);
+  stage(TLight, RD);
 }
 
 void loop() {
+  delay(2000);
+  
   greenToRed(TLIGHT_1);
+  delay(1000);
   redToGreen(TLIGHT_2);
-  delay(2000);          //Traffic flowing, TLight_2
+  
+  delay(2000);
+  
   greenToRed(TLIGHT_2);
+  delay(1000);
   redToGreen(TLIGHT_1);
-  delay(2000);          //Traffic flowing, TLight_1
 }
