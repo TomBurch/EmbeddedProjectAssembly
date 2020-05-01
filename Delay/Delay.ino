0bd5d5581a8c50ca32292d3af46103c8a5ce5317
TomBurch <thomas.burch1306@gmail.com>
2020-05-01 20:39:12 +0100

    Got delay working on Timer1

--- Delay/Delay.ino
+++ Delay/Delay.ino
@@ -1,39 +1,29 @@
 #include "avr/io.h"
-//https://gyazo.com/5cfec7f22a7216b49b74bf7690f03fac
+
 extern "C" {
   void del(unsigned int);
   void displayByte(byte);
   void start();
+  //
+  void init();
+  void set_prescaler();
+  void reset_count();
+  void compare();
+  void assem_delay(unsigned int);
 }
 
 void setup() {
-
   start();
-
-  //TCCR1A = 0;
-  //TCCR1B = 0;
-  //TCNT1 = 0;
-
-  //OCR1A = 31250; // compare match register 16MHz/256/2Hz
-  //TCCR1B |= (1 << CS12); // 256 prescaler
 }
 
 int seconds = 1;
-//ISR(TIMER1_COMPA_vect) // timer compare interrupt service routine
+
 void flash() {
-  //TCNT1 = 0;
-  //TIFR1 = 2;
   displayByte(seconds);
   seconds++;
 }
 
 void loop() {
-  //while ((TIFR1 & 2) == 0) {}
-  del(60000);
+  assem_delay(1000);
   flash();
-  
-  //displayByte(1);
-  //del(1000);
-  //displayByte(2);
-  //del(1000);
 }
