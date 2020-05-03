0b481407e644754b7ee5a9c939cd25eba5bcb3fe
TomBurch <thomas.burch1306@gmail.com>
2020-05-03 20:56:25 +0100

    Comments and organisation

--- Part1-TrafficLight/Part1-TrafficLight.ino
+++ Part1-TrafficLight/Part1-TrafficLight.ino
@@ -1,4 +1,4 @@
-extern "C" {
+extern "C" {      //Assembly function declarations, defined in assem.S
   void start();
   void stage(int);
 }
@@ -7,7 +7,7 @@ void setup() {
   start();
 }
 
-const byte RD    = 0;
+const byte RD    = 0; //Stage - 1
 const byte RD_YE = 1;
 const byte GN    = 2;
 const byte YE    = 3;
